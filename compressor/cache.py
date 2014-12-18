22c3961a774ea763b60f5b654055e0ae2b874cad
John Costa <john.costa@gmail.com>
2014-12-18 12:52:55 -0500

    fix warnings on unit tests

--- compressor/cache.py
+++ compressor/cache.py
@@ -2,10 +2,10 @@ import hashlib
 import os
 import socket
 import time
+import json
 
 from django.core.cache import get_cache
 from django.core.files.base import ContentFile
-from django.utils import simplejson
 from django.utils.encoding import smart_str
 from django.utils.functional import SimpleLazyObject
 from django.utils.importlib import import_module
